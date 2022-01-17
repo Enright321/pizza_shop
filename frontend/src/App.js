355e05840574c257b19c93b6b66a31e7b03df6e1
Enright321 <enrightjoe4@gmail.com>
2022-01-17 07:48:19 -0600

    shipping screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import CategoryScreen from './screens/CategoryScreen';
 import CartScreen from './screens/CartScreen';
+import RegisterScreen from './screens/RegisterScreen';
 import ProductScreen from './screens/ProductScreen';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginScreen from './screens/LoginScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 function App() {
   return (
@@ -14,7 +17,10 @@ function App() {
       <Header />
       <main className='py-3'>
         <Routes>
+          <Route path='/login/shipping' element={<ShippingScreen />} />
           <Route path='/login' element={<LoginScreen />} />
+          <Route path='/register' element={<RegisterScreen />} />
+          <Route path='/profile' element={<ProfileScreen />} />
           <Route path='/:category' element={<CategoryScreen />} />
           <Route path='product/:id' element={<ProductScreen />} />
           <Route path='/cart'>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -8,7 +8,12 @@ import {
 import { categoryListReducer } from './reducers/categoryListReducers';
 import { cartReducer } from './reducers/cartReducers';
 
-import { userLoginReducer } from './reducers/userReducers';
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+  userUpdateProfileReducer,
+} from './reducers/userReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -16,6 +21,9 @@ const reducer = combineReducers({
   categoryList: categoryListReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
@@ -26,8 +34,15 @@ const userInfoFromStorage = localStorage.getItem('userInfo')
   ? JSON.parse(localStorage.getItem('userInfo'))
   : null;
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
+  ? JSON.parse(localStorage.getItem('shippingAddress'))
+  : {};
+
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+  },
   userLogin: { userInfo: userInfoFromStorage },
 };
 
